63654f4ef830a7a02a50c3152db2fddace78029f
stephbak <stephane.bakum@sfr.fr>
2018-12-14 16:06:15 +0100

    exercice7

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$name = 'Stéphane';
+$firstname = 'BAKUM';
+$age = 25;
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <title>php partie1</title>
+</head>
+<body>
+  <p><?= 'Bonjour ' . $name . ' ' . $firstname . ', tu as ' . $age . ' ans.'; ?></p>
+</body>
+</html>
